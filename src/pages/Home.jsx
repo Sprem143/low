70ee41cd62456bea8eeb1e02116386b38ba18aee
Prem Kumar <prem68265@gmail.com>
2024-04-23 00:29:32 +0530

    home page ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,91 @@
 import Carousel from 'react-bootstrap/Carousel';
-export default function Home(){
+export default function Home() {
 
-    return(
+    return (
         <>
-<Carousel>
+            <Carousel>
                 <Carousel.Item>
                     <img src="/static/c-1.jpeg" className='carousel-img' alt="slide-01" text="First Image" />
-                    <Carousel.Caption className='mb-4 c_text'>
+                    {/* <Carousel.Caption className='mb-4 c_text'>
                         <h1 ><b>Service one</b> </h1>
                         <p>Service description</p>
                         <button className='btn'>Know More</button>
 
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                     <img src="/static/c-2.avif" className='carousel-img' alt="slide 02" />
-                    <Carousel.Caption className='mb-4 c_text'>
+                    {/* <Carousel.Caption className='mb-4 c_text'>
                     <h1 ><b>Service one</b> </h1>
                         <p>Service description</p>
                         <button className='btn'>Know More</button>
 
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                     <img src="/static/c-3.jpg" className='carousel-img' alt="slie 03" text="Slide 03" />
-                    <Carousel.Caption className='mb-4 c_text'>
+                    {/* <Carousel.Caption className='mb-4 c_text'>
                     <h1 ><b>Service one</b> </h1>
                         <p>Service description</p>
                         <button className='btn'>Know More</button>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
             </Carousel>
+
+
+            {/* Introduction */}
+            <section className='dfdr w-100 intro_section'>
+                <div className='intro'>
+                    <h2 className='mb-3'>Introduction</h2>
+                    <p>Welcome to the Samrat Choudhary Law Firm, where expertise meets excellence in legal representation. Founded in 2024 by Samrat Choudhary, Esq., our firm is committed to providing unparalleled legal services across a spectrum of practice areas.
+                        <br />
+                        &emsp; &emsp;   With a focus on banking and insurance law, civil litigation, and corporate law, our team brings a wealth of experience and a track record of success to every case we handle. Whether you're a financial institution seeking regulatory guidance, an individual navigating a civil dispute, or a corporation in need of strategic legal counsel, we are here to advocate for your rights and interests with unwavering dedication.
+                        <br />
+
+
+                        &emsp; &emsp; Driven by a commitment to integrity, professionalism, and client satisfaction, Samrat Choudhary Law Firm is your trusted partner in navigating the complexities of the legal landscape. Contact us today to learn how we can assist you with your legal needs. <br />
+                        Beyond our core practice areas, we also offer comprehensive services in criminal law, drawing upon our collective expertise to provide robust defense representation when needed.
+
+                        &emsp; &emsp;  Conveniently located at Radha Kunj near Bargach Chowk, Bari Khanjarpur, Bhagalpur, Bihar, our corporate office serves as a hub for our statewide operations. While based in Bhagalpur, we proudly serve clients across the entirety of Bihar, ensuring access to exceptional legal representation wherever you are in the state.
+
+                        <br />
+                        &emsp; &emsp; At Samrat Choudhary Law Firm, we understand that each legal matter is unique, which is why we approach every case with personalized attention and a tailored strategy. Our team combines deep legal knowledge with innovative thinking to achieve optimal outcomes for our clients.
+                    </p>
+                </div>
+                <div className='photo dfdc jcac p-3'>
+                    <img src="/static/sm.jpg" alt="" width="400" />
+                    <h5 className='mt-2'>Mr. Samrat Chaudhary</h5>
+                </div>
+            </section>
+            <section className="strength ">
+                <h3 className='text-center mb-3'>Our Strength</h3>
+                <div className=" text-container">
+                    <div className='first_str'>
+                        <p>With the values of Honesty and Integrity, Dedication, Transparency in Communication and Diligence and Accountability that we imbibe, we cater to a wide spectrum of clients and are committed to be the finest corporate consultants in India. We represent Big Corporates & Prominent Individuals including Fortune 500 companies and also offer consultants for foreign collaboration.</p>
+                    </div>
+                    <div className='ms-3 mt-4'>
+                        <p>We offer panoply of services to our clients, with dedicated professionals in Corporate Law, Foreign Investment, Employment and Labour laws, Pharma and Healthcare, Bankruptcy, Real Estate, Start-ups, Infrastructure, E-commerce, and many more. We believe in exceeding the expectation of our clients’ needs efficiently, giving them quality service and customized solutions for them to achieve optimum results through our services.</p>
+                    </div>
+                </div>
+            </section>
+
+            {/* get in touch */}
+            <section className='getintouch dfdc p-2 jcac'>
+                <h3 className='text-center mb-4 mt-4'>Get <b>In touch</b></h3>
+                <div className='dfdr se form_cont'>
+                    <div className='me-4'>
+                        <input className ="mb-3" type="text" placeholder='Name' />
+                        <input className ="mb-3" type="text" placeholder='Email' />
+                    </div>
+                    <div >
+                        <input className ="mb-3" type="text" placeholder='Phone' />
+                        <input className ="mb-3" type="text" placeholder='Subject' />
+                    </div>
+
+                </div>
+                <textarea style={{width:"65vw"}} name="" id="" cols="47" rows="5" placeholder='Message' className='p-2'></textarea>
+                <button className='btn btn-primary p-2 mt-3' style={{width:"20vw"}}>Submit</button>
+            </section>
         </>
     )
 }
--- src/pages/component/Header.jsx
+++ src/pages/component/Header.jsx
@@ -13,9 +13,9 @@ export default function Header() {
         <Navbar key={expand} expand={expand} className="bg-body-dark">
           <Container fluid>
             <Navbar.Brand href="#">
-            <div className='logo'>
+              <div className='logo dfdr ac'>
                 <img src="/static/nav_logo.jpeg" className='br-50' width="70" alt="Logo" />
-                <Navbar.Brand href="#home">Samrat Law Firm</Navbar.Brand>
+                <Navbar.Brand href="#home"><h4>Samrat Law Firm</h4></Navbar.Brand>
               </div>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
@@ -26,40 +26,39 @@ export default function Header() {
             >
               <Offcanvas.Header closeButton className='bg-primary'>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`} >
-                 <h2 className="text-white">S.C Law Firm</h2>
+                  <h2 className="text-white">S.C Law Firm</h2>
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                <div className="links_container">
-                <Nav.Link href="#home" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className=" me-2 bi bi-house-door-fill" viewBox="0 0 16 16">
-  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
-</svg><span>Home</span></Nav.Link>
-                <Nav.Link href="#features" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi me-2 bi-file-person-fill" viewBox="0 0 16 16">
-  <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2m-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11"/>
-</svg>About Us</Nav.Link>
-                <Nav.Link href="#features" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="me-2 bi bi-card-checklist" viewBox="0 0 16 16">
-  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
-  <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0"/>
-</svg>Services</Nav.Link>
-                <Nav.Link href="#features" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="me-2 bi bi-arrow-clockwise" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
-  <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
-</svg>Updates</Nav.Link>
-                <Nav.Link href="#pricing" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="me-2 bi bi-telephone-forward-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zm10.761.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708"/>
-</svg>Contact Us</Nav.Link>
-              </div>
-                    
-                 
+                  <div className="links_container">
+                    <Nav.Link href="#home" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className=" me-2 bi bi-house-door-fill" viewBox="0 0 16 16">
+                      <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
+                    </svg><span>Home</span></Nav.Link>
+                    <Nav.Link href="#features" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi me-2 bi-file-person-fill" viewBox="0 0 16 16">
+                      <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2m-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11" />
+                    </svg>About Us</Nav.Link>
+                    <Nav.Link href="#features" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="me-2 bi bi-card-checklist" viewBox="0 0 16 16">
+                      <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z" />
+                      <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0" />
+                    </svg>Services</Nav.Link>
+                    <Nav.Link href="#features" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="me-2 bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
+                      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+                    </svg>Updates</Nav.Link>
+                    <Nav.Link href="#pricing" className='dfdr ac'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="me-2 bi bi-telephone-forward-fill" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zm10.761.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708" />
+                    </svg>Contact Us</Nav.Link>
+                  </div>
+
+
                 </Nav>
-                
+
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
         </Navbar>
       ))}
-
     </>
   )
 }
