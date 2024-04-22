9c3af3fbbbf15560b7f82a37a505a805c38edf8b
Prem Kumar <prem68265@gmail.com>
2024-04-22 19:05:43 +0530

    logo added

--- src/pages/component/Header.jsx
+++ src/pages/component/Header.jsx
@@ -2,20 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-export default function Header(){
+export default function Header() {
 
-    return(
-        <>
-         <Navbar bg="dark" data-bs-theme="dark">
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" className='w-100'>
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav className="me-auto w-100 dfdr ac sb">
+            <div className='logo'>
+              <img src="/static/nav_logo.jpeg" className='br-50' width="70" alt="Logo" />
+              <Navbar.Brand href="#home">Samrat Low Firm</Navbar.Brand>
+            </div>
+            <div className="links_container dfdr">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">About Us</Nav.Link>
+              <Nav.Link href="#features">Services</Nav.Link>
+              <Nav.Link href="#features">Updates</Nav.Link>
+              <Nav.Link href="#pricing">Contact Us</Nav.Link>
+            </div>
           </Nav>
         </Container>
       </Navbar>
-        </>
-    )
+    </>
+  )
 }
