cd54a317906f998e16ae0d03ef7bea82d0f2aaa1
Prem Kumar <prem68265@gmail.com>
2024-04-22 08:02:14 +0530

    navbar included

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import './App.scss'
-
+import Header from './pages/component/Header';
+import Home from './pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   return (
     <>
-    <h1>Hello Samrat</h1> 
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}>
+
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.scss'
-
+import './index.scss';
+import Footer from './pages/component/Footer.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
 )
