083b9af83cbee3c9c8c71f5119756867faf74036
Prem Kumar <prem68265@gmail.com>
2024-04-22 21:05:02 +0530

    coresol added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,36 @@
-
+import Carousel from 'react-bootstrap/Carousel';
 export default function Home(){
 
     return(
         <>
-        <h1>Home Page</h1>
+<Carousel>
+                <Carousel.Item>
+                    <img src="/static/c-1.jpeg" className='carousel-img' alt="slide-01" text="First Image" />
+                    <Carousel.Caption className='mb-4 c_text'>
+                        <h1 ><b>Service one</b> </h1>
+                        <p>Service description</p>
+                        <button className='btn'>Know More</button>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src="/static/c-2.avif" className='carousel-img' alt="slide 02" />
+                    <Carousel.Caption className='mb-4 c_text'>
+                    <h1 ><b>Service one</b> </h1>
+                        <p>Service description</p>
+                        <button className='btn'>Know More</button>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src="/static/c-3.jpg" className='carousel-img' alt="slie 03" text="Slide 03" />
+                    <Carousel.Caption className='mb-4 c_text'>
+                    <h1 ><b>Service one</b> </h1>
+                        <p>Service description</p>
+                        <button className='btn'>Know More</button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </>
     )
 }
